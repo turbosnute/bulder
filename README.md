a38f8257e7f998d12464b9405db4db2cf9db5485
Øyvind Nilsen <oyvind.nilsen@gmail.com>
2022-04-30 22:17:08 +0200

    lot of work with stats

--- README.md
+++ README.md
@@ -17,6 +17,8 @@ Apache, PHP, MariaDB and Bootstrap.
 ## Needs fixing
  * grade database must be created and populated in setup.php
  * delete functionality on edit sends.
+ * confirmation dialog on deletes?
  * diary (github contributors style?)
  * change locale on calendar so weeks start on monday.
  * google auth
+ * User classes (all should probably not be admins).
--- web/stats.php
+++ web/stats.php
@@ -14,7 +14,7 @@
           # Number of sends
           $query = "SELECT COUNT(send_id) AS 'count' FROM `bulder_send` WHERE `user_id` = '1' LIMIT 1";
           $result = mysqli_query($conn, $query);
-          
+
           if (mysqli_num_rows($result) > 0) {
             $row = mysqli_fetch_assoc($result);
             $send_count = $row['count'];
@@ -25,24 +25,58 @@
           if ($send_count == 0) {
             echo "Register more sends to see statistics.";
           } else {
-            echo "Send Count: $send_count";
             # Most number of sends in a day 
             $query = "SELECT COUNT(DATE) AS 'count', `date` FROM `bulder_send` WHERE `user_id` = '1' GROUP BY DATE ORDER BY COUNT LIMIT 1";
 
             # grade stats and favorite grade.
-            $query = "SELECT COUNT(grade) AS 'count' , grade FROM `bulder_send` WHERE `user_id` = '1' GROUP BY grade ORDER BY 'count' LIMIT 1;";
-            $result = mysqli_query($conn, $query);
-            $row = mysqli_fetch_assoc($result);
-            //$favorite_grade = $row['grade'];
-            //echo "Favorite Grade: $favorite_grade";
+            $query = "SELECT COUNT(*) AS 'count', `bulder_send`.`grade`, `bulder_grade`.hardness
+                      FROM `bulder_send`
+                      INNER JOIN `bulder`.`bulder_grade` ON `bulder_send`.grade = `bulder_grade`.grade
+                      WHERE `user_id` = 1
+                      GROUP BY `grade`
+                      ORDER BY `count` DESC";
 
+            $result = mysqli_query($conn, $query);
+            $arr = mysqli_fetch_all($result, MYSQLI_ASSOC);
+            $usersendcount = array_sum(array_column($arr,'count'));
+
+            ?>
+<div class="card mb-3">
+      <div class="card-body">
+        <p class="text-muted">Number of sends logged</p>
+        <p class="h1"><?php echo "$send_count"; ?></p>
+
+            <?php
+            foreach ($arr as &$value) {
+              $grade = $value['grade'];
+              $count = $value['count'];
+              echo "<a class='badge bg-$grade'>$count</a> ";
+            }
+            ?>
+            </div></div>
+
+            <?php
+            $favorite_grade = $arr[0]['grade'];
+            $favorite_grade_count = $arr[0]['count'];
+/*
+            while($row = mysqli_fetch_assoc($result)) {
+              echo "<tr><td>".$row["name"]."</td><td>".$row["email"]."</td></tr>";
+            }
+*/
             # favorite gym.
-            $query = "SELECT `bulder_send`.`crag_id`, `bulder_crag`.`name`, COUNT(`bulder_send`.`crag_id`) AS 'count' FROM `bulder_send` INNER JOIN `bulder`.`bulder_crag` ON `bulder_send`.crag_id = `bulder_crag`.crag_id WHERE `user_id` = '1' GROUP BY crag_id ORDER BY 'count' ASC LIMIT 1";
+            $query = "SELECT DISTINCT COUNT(`date`) AS times_visited, `bulder_crag`.`name`, `bulder_crag`.`city`, `bulder_send`.crag_id
+                      FROM `bulder_send`
+                      INNER JOIN `bulder`.`bulder_crag` ON `bulder_send`.crag_id = `bulder_crag`.crag_id
+                      WHERE `user_id` = '1'
+                      GROUP BY `crag_id`
+                      ORDER BY `times_visited` DESC
+                      LIMIT 1";
+
             $result = mysqli_query($conn, $query);
             $row = mysqli_fetch_assoc($result);
             $favorite_crag_name = $row['name'];
-            $favorite_crag_count = $row['count'];
-            echo "Favorite Gym: $favorite_crag_name ($favorite_crag_count sends).";
+            $favorite_crag_city = $row['city'];
+            $favorite_crag_visit_count = $row['times_visited'];
 
             # hardest send.
             $query = "SELECT `bulder_send`.`crag_id`, `bulder_send`.`date`, `bulder_crag`.crag_id, `bulder_crag`.name,`bulder_send`.style, `bulder_grade`.`hardness`, `bulder_send`.grade
@@ -57,14 +91,34 @@
             $hardest_send_crag = $row['name'];
             $hardest_send_date = $row['date'];
             $hardest_send_grade = $row['grade'];
-            echo "Hardest Send: $hardest_send_grade ($hardest_send_date at $hardest_send_crag)";
           }
 
           mysqli_close($conn);
         ?>
-   </table>
-  </div>
+<div class="card mb-3">
+      <div class="card-body">
+        <p class="text-muted">Hardest Send</p>
+        <p class="h1"><?php echo "$hardest_send_grade"; ?></p>
 
+        <p class="card-text"><small class="text-muted"><i class="bi bi-calendar-event"></i> <?php echo $hardest_send_date;?><br /><i class="bi bi-geo-alt"></i> <?php echo $hardest_send_crag;?></small></p>
+      </div>
+</div>
+
+<div class="card mb-3">
+      <div class="card-body">
+        <p class="text-muted">Favorite Grade</p>
+        <p class="h1"><?php echo "$favorite_grade"; ?></p>
+        <p class="card-text"><small class="text-muted"><i class="bi bi-123"></i><?php echo " $favorite_grade_count sends logged"; ?></small></p>
+      </div>
+</div>
+<div class="card mb-3">
+      <div class="card-body">
+        <p class="text-muted">Favorite Gym</p>
+        <p class="h1"><?php echo "$favorite_crag_name"; ?></p>
+        
+        <p class="card-text"><small class="text-muted"><i class="bi bi-bicycle"></i> <?php echo "Visited $favorite_crag_visit_count times";?><br /><i class="bi bi-geo-alt"></i> <?php echo $favorite_crag_city;?></small></p>
+      </div>
+</div>
     <style>
       .bd-placeholder-img {
         font-size: 1.125rem;
