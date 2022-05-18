2e7be950084973c512fc667e9515716e14962145
Øyvind Nilsen <oyvind.nilsen@gmail.com>
2022-05-18 19:23:03 +0200

    hamburger button is back

--- web/top.php
+++ web/top.php
@@ -39,7 +39,6 @@ if (isset($_SESSION['user_class'])) {
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/bootstrap-datepicker/1.8.0/css/bootstrap-datepicker3.min.css" />
-    <!--<script src="https://maps.googleapis.com/maps/api/js?v=3.exp&key=AIzaSyCfRCncd6qM9hMR5em49g9BAD1s7dQXhZk&libraries=places"></script>-->
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.8.1/font/bootstrap-icons.css">
     <!-- Font Awesome -->
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0/css/all.min.css" />
@@ -58,35 +57,35 @@ if (isset($_SESSION['user_class'])) {
         <div class="container-fluid">
             <a class="navbar-brand" href="index.php">Bulder</a>
             <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarCollapse" aria-controls="navbarCollapse" aria-expanded="false" aria-label="Toggle navigation">
-            <span class="navbar-toggler-icon"></span>
+              <span class="navbar-toggler-icon"></span><i class="fas fa-bars"></i>
             </button>
             <div class="collapse navbar-collapse" id="navbarCollapse">
-            <ul class="navbar-nav me-auto mb-2 mb-md-0">
+              <ul class="navbar-nav me-auto mb-2 mb-md-0">
                 <li class="nav-item">
-                <a class="nav-link<?php if ($site == 'logbook') { echo ' active';}?>" aria-current="page" href="index.php">Logbook</a>
+                  <a class="nav-link<?php if ($site == 'logbook') { echo ' active';}?>" aria-current="page" href="index.php">Logbook</a>
                 </li>
                 <li class="nav-item">
-                <a class="nav-link<?php if ($site == 'diary') { echo ' active';}?>" aria-current="page" href="diary.php">Diary</a>
+                  <a class="nav-link<?php if ($site == 'diary') { echo ' active';}?>" aria-current="page" href="diary.php">Diary</a>
                 </li>
                 <li class="nav-item">
-                    <a class="nav-link<?php if ($site == 'stats') { echo ' active';}?>" href="stats.php">Stats</a>
+                  <a class="nav-link<?php if ($site == 'stats') { echo ' active';}?>" href="stats.php">Stats</a>
                 </li>
                 <?php
                 if ($user_class == 'admin') {
                 ?>
                 <li class="nav-item">
-                <a class="nav-link<?php if ($site == 'gyms') { echo ' active';}?>" href="crags.php">Gyms</a>
+                  <a class="nav-link<?php if ($site == 'gyms') { echo ' active';}?>" href="crags.php">Gyms</a>
                 </li>
                 <li class="nav-item">
-                <a class="nav-link<?php if ($site == 'users') { echo ' active';}?>" href="users.php">Users</a>
+                  <a class="nav-link<?php if ($site == 'users') { echo ' active';}?>" href="users.php">Users</a>
                 </li>
                 <?php
                 }
                 ?>
                 <li class="nav-item">
-                    <a class="nav-link" href="processlogout.php">Log Out (<?php echo $user_name ?>)</a>
+                  <a class="nav-link" href="processlogout.php">Log Out (<?php echo $user_name ?>)</a>
                 </li>
-            </ul>
+              </ul>
             </div>
         </div>
     </nav>
